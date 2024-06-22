bab7ee088aee128d3ff6f433f225c9a6b2e263cc
Murat Bukarbaev <85822522+supreme-clientele@users.noreply.github.com>
2024-06-22 13:32:29 +0600

    Home Work 5

--- js/lesson.js
+++ js/lesson.js
@@ -67,3 +67,51 @@ tabParent.onclick = (event) => {
         });
     };
 };
+
+
+// CONVERTER
+const usdInput = document.querySelector('#usd');
+const somInput = document.querySelector('#som');
+const eurInput = document.querySelector('#eur');
+
+const converter = (element, targetElement) => {
+    element.oninput = () => {
+        const request = new XMLHttpRequest()
+        request.open('GET', '../data/converter.json')
+        request.setRequestHeader('Content-type', 'application/json')
+        request.send()
+
+        request.onload = () => {
+            const data = JSON.parse(request.response)
+
+            if (element.id === 'som') {
+                usdInput.value = (element.value * data.som_to_usd).toFixed(2);
+                eurInput.value = (element.value * data.som_to_eur).toFixed(2);
+            }
+
+            if (element.id === 'usd') {
+                eurInput.value = (element.value * data.usd_to_eur).toFixed(2);
+                somInput.value = (element.value / data.som_to_usd).toFixed(2);
+            }
+
+            if (element.id === 'eur') {
+                somInput.value = (element.value / data.som_to_eur).toFixed(2);
+                usdInput.value = (element.value / data.usd_to_eur).toFixed(2);
+            }
+
+            if (element.value === '') {
+                somInput.value = '';
+                usdInput.value = '';
+                eurInput.value = '';
+            }
+        };
+    };
+};
+
+converter(somInput, usdInput);
+converter(usdInput, somInput);
+converter(somInput, eurInput);
+converter(eurInput, somInput);
+converter(usdInput, eurInput);
+converter(eurInput, usdInput);
+
